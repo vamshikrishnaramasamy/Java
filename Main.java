1419099dcdb62cd479119edb7e87d979e7c1e0ec
vamshikrishnaramasamy <161273586+vamshikrishnaramasamy@users.noreply.github.com>
2025-06-18 12:40:01 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        boolean isStudent = false;
+        if (isStudent) {
+            System.out.println("You are a student!");
+        }
+        else {
+            System.out.println("You are not a student!");
+        }
+    }
+}
