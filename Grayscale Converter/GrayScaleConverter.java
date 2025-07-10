14a5964295873343908830537b5394cf3c3c0b94
vamshikrishnaramasamy <161273586+vamshikrishnaramasamy@users.noreply.github.com>
2025-07-10 13:34:36 -0700

    Add files via upload

---
+++ Grayscale Converter/GrayScaleConverter.java
@@ -0,0 +1,44 @@
+import edu.duke.*;
+import java.io.*;
+
+public class GrayScaleConverter {
+    //I started with the image I wanted (inImage)
+    public ImageResource makeGray(ImageResource inImage) {
+        //I made a blank image of the same size
+        ImageResource outImage = new ImageResource(inImage.getWidth(), inImage.getHeight());
+        //for each pixel in outImage
+        for (Pixel pixel: outImage.pixels()) {
+            //look at the corresponding pixel in inImage
+            Pixel inPixel = inImage.getPixel(pixel.getX(), pixel.getY());
+            //compute inPixel's red + inPixel's blue + inPixel's green
+            //divide that sum by 3 (call it average)
+            int average = (inPixel.getRed() + inPixel.getBlue() + inPixel.getGreen())/3;
+            //set pixel's red to average
+            pixel.setRed(average);
+            //set pixel's green to average
+            pixel.setGreen(average);
+            //set pixel's blue to average
+            pixel.setBlue(average);
+        }
+        //outImage is your answer
+        return outImage;
+    }
+
+    public void selectAndConvert () {
+        DirectoryResource dr = new DirectoryResource();
+        for (File f : dr.selectedFiles()) {
+            ImageResource inImage = new ImageResource(f);
+            ImageResource gray = makeGray(inImage);
+            String fName = inImage.getFileName();
+            gray.setFileName(f.getParentFile()+"/"+"gray-"+fName);
+            gray.draw();
+            gray.save();
+        }
+    }
+
+    public void testGray() {
+        ImageResource ir = new ImageResource();
+        ImageResource gray = makeGray(ir);
+        gray.draw();
+    }
+}
